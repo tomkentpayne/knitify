09bd84324e48edd383e7fb50a3a70ccd3239c05e
tomkentpayne <tomkentpayne@hotmail.com>
2015-07-05 23:28:19 +0100

    argparser for bitmap path

--- source/knitify.py
+++ source/knitify.py
@@ -6,13 +6,15 @@ author: Thomas Payne
 email: tomkentpayne@hotmail.com
 licence: GPL v2
 """
-import sys
 from argparse import ArgumentParser
 from pattern import Pattern
 
-def parse_args():
-    pass
+def parse_sys_args():
+    parser = ArgumentParser(description='Generate knitting pattern from a bitmap image')
+    parser.add_argument('bitmap' ,action='store')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-
+    bitmap_path = parse_sys_args().bitmap
     pattern = Pattern()
+    print(bitmap_path)
