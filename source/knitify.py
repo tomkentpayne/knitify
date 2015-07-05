215bf602c38ad2208b2abdbbe581e0167ca13955
tomkentpayne <tomkentpayne@hotmail.com>
2015-07-05 23:09:31 +0100

    argparser barebones

--- source/knitify.py
+++ source/knitify.py
@@ -7,8 +7,11 @@ email: tomkentpayne@hotmail.com
 licence: GPL v2
 """
 import sys
+from argparse import ArgumentParser
 from pattern import Pattern
 
+def parse_args():
+    pass
 
 if __name__ == '__main__':
 
