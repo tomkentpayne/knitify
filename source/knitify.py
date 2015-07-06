242e072e32f2302dcf15d844a31d1ea6b577c856
tomkentpayne <tomkentpayne@hotmail.com>
2015-07-06 18:43:25 +0100

    Bitmap loaded into scipy image

--- source/knitify.py
+++ source/knitify.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-# knitify.py
 """ Generate a pdf knittinf pattern + chart from a bitmap image """
 __author__ = 'Thomas Payne'
 __email__ = 'tomkentpayne@hotmail.com'
 __copyright__ = 'Copyright © 2015 Thomas Payne'
 __licence__ = 'GPL v2'
+
 from argparse import ArgumentParser
 from pattern import Pattern
 
 def parse_sys_args():
+    ''' Parse arguments for bitmap path '''
     parser = ArgumentParser(description='Generate knitting pattern from a bitmap image')
     parser.add_argument('bitmap' ,action='store')
     return parser.parse_args()
@@ -17,4 +17,5 @@ if __name__ == '__main__':
     args = parse_sys_args()
     bitmap_path = args.bitmap
     pattern = Pattern()
+    #Print arg for debug purposes
     print(bitmap_path)
--- source/pattern.py
+++ source/pattern.py
@@ -3,6 +3,10 @@ __author__ = 'Thomas Payne'
 __email__ = 'tomkentpayne@hotmail.com'
 __copyright__ = 'Copyright © 2015 Thomas Payne'
 __licence__ = 'GPL v2'
+
+import numpy as np
+from scipy import misc
+
 class Pattern(object):
     """A knitting pattern object generated from a bmp image"""
     def __init__(self):
@@ -12,4 +16,10 @@ class Pattern(object):
         """Human-readable form of the pattern, i.e. to print and knit from"""
     def load_bitmap(self, bitmap_path):
         """Populate 2D stitches array from given bitmap"""
-        pass
+        self.bitmap = misc.imread(bitmap_path)
+        if self.bitmap is None:
+            raise FileNotFoundError('Image not found.')
+        else:
+            print("YAY")
+        #example np array 5x5
+        image_array = np.array(np.zeros([5,5]))
